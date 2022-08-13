04f9ec8bb562172c006c84f24347b578feb05d58
Youssef Ragaey <youssefragaa3@gmail.com>
2022-08-13 21:20:56 +0200

    Merge with mobile-list branch

---
+++ app.js
@@ -0,0 +1,9 @@
+document.getElementsByClassName('list')[0].addEventListener('click', function () {
+    document.getElementsByClassName("sidebar")[0].style.display == "none" ?
+    document.getElementsByClassName("sidebar")[0].style.display = "block" :
+    document.getElementsByClassName("sidebar")[0].style.display = "none";
+})
+
+document.querySelector('.sidebar a:first-child').addEventListener('click', function () {
+    document.getElementsByClassName("sidebar")[0].style.display = "none";
+})
