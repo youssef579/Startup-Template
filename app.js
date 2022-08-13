bdfc21dd482607195b06d0ac77b140720b5119ca
Youssef Ragaey <youssefragaa3@gmail.com>
2022-08-13 21:36:05 +0200

    Update JS file

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+document.getElementsByClassName("sidebar")[0].style.display = "none";
+
 document.getElementsByClassName('list')[0].addEventListener('click', function () {
     document.getElementsByClassName("sidebar")[0].style.display == "none" ?
     document.getElementsByClassName("sidebar")[0].style.display = "block" :
